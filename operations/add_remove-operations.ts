c66658589bc9bd45582c376a03845736a5088a0e
sushilp <sushilp3@hexaware.com>
2024-02-13 10:37:43 +0530

    update with suresh changes

--- operations/add_remove-operations.ts
+++ operations/add_remove-operations.ts
@@ -2,6 +2,13 @@
  * 
  */
 export interface add_remove_actions{
-    addElement: () => void;
-    getAddedElements: () => number;
+    addElement: () => Promise<void>;
+    getAddedElements: () => Promise<string[]>;
+    /**
+     * 
+     * @param position This is an optional position to mention which element position should be remove , if not provided the first element will be removed
+     * @returns 
+     */
+    removeElement: (position?: number) => Promise<void>; 
+    
 }
